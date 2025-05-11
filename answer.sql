9d71d2f28e8d328febc64833b866000f18e96ddb
Ze2-lucy <zethulucysitole@gmail.com>
2025-05-12 00:09:48 +0200

    Added Books table

--- answer.sql
+++ answer.sql
@@ -4,3 +4,12 @@ CREATE TABLE Authors (
     birthdate DATE,
     nationality VARCHAR(100)
 );
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    genre VARCHAR(100),
+    publication_year INT,
+    author_id INT,
+    UNIQUE(title),
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id) ON DELETE SET NULL
+);
